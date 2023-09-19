f6725fdc33ba633ea13d7468c43ddf09c6f93c74
RytoryQA <vikaryzhkina89@gmail.com>
2023-09-19 21:34:18 +0400

    add Radio and RadioTest modification

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -2,63 +2,118 @@ package org.example;
 
 public class Radio {
 
+    private int allChannel = 10;
+    private int minChannel = 0;
+    private int maxChannel = allChannel - 1;
     private int channel;
+    private int minVolume = 0;
+    private int maxVolume = 100;
     private int volume;
 
     public int getChannel() {
         return channel;
     }
 
+    public int getAllChannel() {
+        return allChannel;
+    }
+
+    public int getMinChannel() {
+        return minChannel;
+    }
+
+    public int getMaxChannel() {
+        return maxChannel;
+    }
+
     public void setChannel(int newChannel) {
 
-        if (newChannel < 0) {
+        if (newChannel < minChannel) {
             return;
         }
-        if (newChannel > 9) {
+        if (newChannel > maxChannel) {
             return;
         }
 
-        channel = newChannel;
+        this.channel = newChannel;
+    }
+
+    public void setAllChannel(int newAllChannel) {
+        if (newAllChannel <= minChannel) {
+            return;
+        }
+        this.allChannel = newAllChannel;
+    }
+
+    public void setMaxChannel(int newMaxChannel) {
+        if (newMaxChannel <= minChannel) {
+            return;
+        }
+        if (newMaxChannel > allChannel){
+            return;
+        }
+        this.maxChannel = newMaxChannel;
+    }
+
+
+    public Radio(int allChannel) {
+        this.allChannel = allChannel;
+        this.maxChannel = allChannel - 1;
+
+    }
+    public Radio(){
+        this.allChannel = 10;
+        this.minChannel = 0;
+        this.maxChannel = 9;
+        this.channel = this.minChannel;
     }
 
     public void increaseChannel() {
         int next = channel + 1;
 
         setChannel(next);
-        if (next > 9) {
-            channel = 0;
+        if (next > maxChannel) {
+            channel = minChannel;
         }
     }
 
     public void decreaseChannel() {
         int prev = channel - 1;
         setChannel(prev);
-        if (prev < 0) {
-            channel = 9;
+        if (prev < minChannel) {
+            channel = maxChannel;
         }
     }
 
+    // дальше про громкость
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
 
     public int getVolume() {
         return volume;
     }
 
     public void setVolume(int newVolume) {
-        if (newVolume < 0) {
+        if (newVolume < minVolume) {
             return;
         }
-        if (newVolume > 100) {
+        if (newVolume > maxVolume) {
             return;
         }
-        volume = newVolume;
+        this.volume = newVolume;
     }
 
     public void increaseVolume() {
         int upVolume = volume + 1;
 
         setVolume(upVolume);
-        if (upVolume > 100) {
-            volume = 100;
+        if (upVolume > maxVolume) {
+           volume = maxVolume;
         }
     }
 
@@ -66,8 +121,8 @@ public class Radio {
         int downVolume = volume - 1;
 
         setVolume(downVolume);
-        if (downVolume < 0) {
-            volume = 0;
+        if (downVolume < minVolume) {
+            this.volume = minVolume;
         }
     }
 
